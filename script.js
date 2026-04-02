3703e342c20dff9bb5d272166bb28d250cb00a6a
JoachimBograd <Joachimbbograd@live.com>
2026-04-02 14:28:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const messageText = "Hvis du stadig gerne vil have kærlighed, så er jeg her for dig. Jeg savner dig. Meget.";
+const messages = [
+    "Hvis du stadig gerne vil have kærlighed, så er jeg her for dig. Jeg savner dig. Meget.",
+    "Hvis du stadig gerne vil have kærlighed, så er jeg her for dig. Jeg savner dig. Meget.",
+    "Hvis du stadig gerne vil have kærlighed, så er jeg her for dig. Jeg savner dig. Meget.",
+    "Hvis du stadig gerne vil have kærlighed, så er jeg her for dig. Jeg savner dig. Meget.",
+    "Hvis du stadig gerne vil have kærlighed, så er jeg her for dig. Jeg savner dig. Meget.",
+    "Hvis du stadig gerne vil have kærlighed, så er jeg her for dig. Jeg savner dig. Meget.",
+    "Hvis du stadig gerne vil have kærlighed, så er jeg her for dig. Jeg savner dig. Meget.",
+    "Hvis du stadig gerne vil have kærlighed, så er jeg her for dig. Jeg savner dig. Meget.",
+    "Hvis du stadig gerne vil have kærlighed, så er jeg her for dig. Jeg savner dig. Meget.",
+    "Hvis du stadig gerne vil have kærlighed, så er jeg her for dig. Jeg savner dig. Meget."
+];
 
+// Hent knap og tekstområde
 const button = document.getElementById("loveButton");
 const message = document.getElementById("message");
 
+// Tilføj klik-event til knappen
 button.addEventListener("click", () => {
-    message.textContent = messageText;
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    message.textContent = randomMessage;
 });
